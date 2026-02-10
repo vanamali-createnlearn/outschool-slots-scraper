31b95fa82dd1f1d44ce1cc697ad9f50f4905f618
vanamali-createnlearn <vanamali@createnlearn.com>
2026-02-10 12:43:55 +0530

    Implement normalize_slots function for meetings
    Added a function to normalize meeting time slots into a set of weekday and time tuples.

--- scraper/normalize.py
+++ scraper/normalize.py
@@ -1,1 +1,17 @@
+from dateutil import parser
+from collections import defaultdict
 
+def normalize_slots(meetings):
+    slots = set()
+
+    for start, end in meetings:
+        start_dt = parser.isoparse(start)
+        end_dt = parser.isoparse(end)
+
+        weekday = start_dt.strftime("%A")
+        start_time = start_dt.strftime("%H:%M")
+        end_time = end_dt.strftime("%H:%M")
+
+        slots.add((weekday, start_time, end_time))
+
+    return slots
