b399ed19e719676e33cbd49262997bcdb667a454
vanamali-createnlearn <vanamali@createnlearn.com>
2026-02-10 12:44:14 +0530

    Refactor compare_slots to compare inhouse and competitor slots
    Refactor compare_slots function to handle missing and extra slots.

--- scraper/compare.py
+++ scraper/compare.py
@@ -1,42 +1,13 @@
-import json
-from fetch import fetch_course_slots
-from normalize import normalize_slots
-from compare import compare_slots
-from sheets import write_competitor_matrices, write_comparisons
+def compare_slots(inhouse_slots, competitor_data):
+    competitor_union = set()
 
-def load_courses():
-    with open("courses.json", "r") as f:
-        return json.load(f)["courses"]
+    for course in competitor_data.values():
+        competitor_union |= course["slots"]
 
-def main():
-    courses = load_courses()
+    missing = competitor_union - inhouse_slots
+    extra = inhouse_slots - competitor_union
 
-    competitor_data = {}
-    inhouse_data = {}
-
-    for course in courses:
-        raw_slots = fetch_course_slots(course["url"])
-        weekly_slots = normalize_slots(raw_slots)
-
-        if course["nature"] == "competitor":
-            competitor_data[course["id"]] = {
-                "name": course["name"],
-                "slots": weekly_slots
-            }
-        elif course["nature"] == "inhouse":
-            inhouse_data[course["id"]] = {
-                "name": course["name"],
-                "slots": weekly_slots
-            }
-
-    write_competitor_matrices(competitor_data)
-
-    for inhouse in inhouse_data.values():
-        comparisons = compare_slots(
-            inhouse["slots"],
-            competitor_data
-        )
-        write_comparisons(inhouse["name"], comparisons)
-
-if __name__ == "__main__":
-    main()
+    return {
+        "missing": missing,
+        "extra": extra
+    }
